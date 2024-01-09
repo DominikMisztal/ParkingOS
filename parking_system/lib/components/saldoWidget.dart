be983eb060b9d373e414ca1b2806c29fea0f1a6b
MartynaKalemba <Martynaakalemba@gmail.com>
2024-01-09 22:17:38 +0100

    Implementing charge oprion for saldo

--- parking_system/lib/components/saldoWidget.dart
+++ parking_system/lib/components/saldoWidget.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-class Saldo extends StatelessWidget {
-  final int saldo;
+class Saldo extends StatefulWidget {
+  final saldo;
+
+  //double saldoChange = 0;
   const Saldo({super.key, required this.saldo});
 
+  @override
+  State<Saldo> createState() => _SaldoState();
+}
+
+class _SaldoState extends State<Saldo> {
   void _chargeSaldo() {}
 
   @override
@@ -24,14 +31,85 @@ class Saldo extends StatelessWidget {
             height: 100,
             child: Center(
               child: Text(
-                '${saldo}zł',
+                '${widget.saldo}zł',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
             ),
           ),
         ),
-        ElevatedButton(onPressed: _chargeSaldo, child: Text('Charge'))
+        ElevatedButton(
+          onPressed: () async {
+            double? val = await showDialog<double>(
+              context: context,
+              builder: (context) => ChargeDialog(
+                val: widget.saldo,
+              ),
+            );
+            setState(() {
+              //Todo muszę to poprawić
+              // widget.saldo += val!;
+            });
+            print('Dialog one returned value ---> $val');
+          },
+          child: const Text('Charge'),
+        ),
       ],
+    );
+  }
+}
+
+class ChargeDialog extends StatefulWidget {
+  @override
+  _ChargeDialogState createState() => _ChargeDialogState();
+
+  final double val;
+
+  ChargeDialog({
+    required this.val,
+  });
+}
+
+class _ChargeDialogState extends State<ChargeDialog> {
+  double value = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    value = widget.val;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Column(
+        children: [
+          TextField(
+            style: TextStyle(color: Colors.white60),
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Enter a number',
+            ),
+            onChanged: (val) {
+              setState(() {
+                // Update enteredNumber when the text changes
+                value = double.tryParse(val) ?? 0;
+              });
+            },
+          ),
+          const SizedBox(height: 15),
+          ElevatedButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.pop(context, value),
+            child: const Text('Charge'),
+          ),
+        ],
+      ),
     );
   }
 }
