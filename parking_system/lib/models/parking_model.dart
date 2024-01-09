a8d68767333f2705d7255b97683c74fed198897b
MartynaKalemba <Martynaakalemba@gmail.com>
2024-01-09 17:39:29 +0100

    Search bar for park_finder is working

--- parking_system/lib/models/parking_model.dart
+++ parking_system/lib/models/parking_model.dart
@@ -1,13 +1,17 @@
 class Parking {
+  String parkingId;
   String name;
   String adress;
   int floors;
   int spotPerFloor;
+  double price; //change to something more sophisticated
 
-  Parking(this.name, this.adress, this.floors, this.spotPerFloor);
+  Parking(this.parkingId, this.name, this.adress, this.floors,
+      this.spotPerFloor, this.price);
 
   Map<String, dynamic> toMap() {
     return {
+      'parkingId': parkingId,
       'name': name,
       'adress': adress,
       'floors': floors,
@@ -17,10 +21,12 @@ class Parking {
 
   factory Parking.fromMap(Map<String, dynamic> map) {
     return Parking(
+      map['parkingId'] ?? '',
       map['name'] ?? '',
       map['adress'] ?? '',
       map['floors'] ?? 0,
       map['spotPerFloor'] ?? 0,
+      map['price'] ?? 0,
     );
   }
 }
--- parking_system/lib/parkfinder_page.dart
+++ parking_system/lib/parkfinder_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:parking_system/components/myCustomTextField.dart';
+import 'package:parking_system/models/parking_model.dart';
+import 'package:parking_system/models/spot_model.dart';
 
 class Parkfinder extends StatefulWidget {
   const Parkfinder({super.key});
@@ -9,11 +11,49 @@ class Parkfinder extends StatefulWidget {
 }
 
 class _ParkfinderState extends State<Parkfinder> {
-  TextEditingController controller = TextEditingController();
+  late Parking currentParking;
+  List<Spot> filteredSpaces = [];
+  @override
+  void initState() {
+    filteredSpaces = [];
+    super.initState();
+  }
+
+  List<Parking> parkings = [
+    Parking('1', 'GreatParking', 'Boat 53-590', 2, 10, 100),
+    Parking('2', 'FineParking', 'Zgierz 53-590', 2, 10, 0)
+  ];
+  List<Spot> spots = [
+    Spot('1', 1, 1, false),
+    Spot('1', 1, 2, true),
+    Spot('1', 1, 3, false),
+    Spot('1', 1, 4, false),
+    Spot('1', 2, 1, false),
+    Spot('1', 2, 2, true),
+    Spot('1', 2, 3, true),
+    Spot('1', 2, 4, false),
+    Spot('2', 2, 1, false),
+    Spot('2', 2, 2, true),
+    Spot('2', 2, 3, true),
+    Spot('2', 2, 4, false),
+  ];
+
+  void filterSpaces(Parking parking) {
+    List<Spot> searchResult = spots
+        .where((spot) => !spot.isTaken && spot.parkingId == parking.parkingId)
+        .toList();
+
+    setState(() {
+      filteredSpaces = searchResult;
+    });
+  }
+
+  TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
+
     return Stack(alignment: AlignmentDirectional.center, children: [
       Container(
         color: Colors.white,
@@ -26,18 +66,16 @@ class _ParkfinderState extends State<Parkfinder> {
         width: width / 3,
         child: Column(
           children: [
-            Text('Search for your Parking'),
-            MyCustomTextField(
-              controller: controller,
-              labelText: 'Find parking',
-              obscureText: false,
+            const Text(
+              'Search for your Parking',
+              style: TextStyle(
+                  color: Colors.white70,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32),
             ),
-
             const SizedBox(
               height: 64,
             ),
-            //Todo - spytać chlopakow, jak to robimy
-
             SearchAnchor(
                 builder: (BuildContext context, SearchController controller) {
               return SearchBar(
@@ -54,18 +92,37 @@ class _ParkfinderState extends State<Parkfinder> {
               );
             }, suggestionsBuilder:
                     (BuildContext context, SearchController controller) {
-              return List<ListTile>.generate(5, (int index) {
-                final String item = 'Parking $index';
+              return List<ListTile>.generate(parkings.length, (int index) {
+                final Parking item = parkings[index];
                 return ListTile(
-                  title: Text(item),
+                  title: Text(item.name),
                   onTap: () {
                     setState(() {
-                      controller.closeView(item);
+                      currentParking = item;
+                      filterSpaces(item);
+                      controller.closeView(item.name);
                     });
                   },
                 );
               });
             }),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredSpaces.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    textColor: Colors.white60,
+                    title: Text(
+                        'Floor: ${filteredSpaces[index].floor} Place_num: ${filteredSpaces[index].number} Price: ${currentParking.price} '),
+                    subtitle: Text(
+                        filteredSpaces[index].isTaken ? 'Occupied' : 'Free'),
+                    onTap: () {
+                      //Navigate to payment
+                    },
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
